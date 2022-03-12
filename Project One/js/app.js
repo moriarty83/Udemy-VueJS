6ee9d9fbfef24c2345c641f73a430c42e14662d5
moriarty83 <moriarty83@gmail.com>
2022-03-12 11:52:56 -0500

    project one, validate names & show list

--- Project One/js/app.js
+++ Project One/js/app.js
@@ -4,6 +4,14 @@ const app = Vue.createApp({
             state: true,
             inputName: "",
             names: [],
+            error: "error text",
+            showError: false,
+        }
+    },
+
+    computed:{
+        isReady(){
+            return this.names.length > 1 ? true : false
         }
     },
 
@@ -12,7 +20,7 @@ const app = Vue.createApp({
             const userName = this.inputName;
             if(this.validate(userName)){
                 this.names.push(userName)
-                this.inpuName = ""
+                this.inputName = ""
                 console.log(this.names)
             }
             else{
@@ -21,7 +29,33 @@ const app = Vue.createApp({
             }
         },
         validate(name){
+            this.error = "";
+            this.showError = false;
+            if(name.length < 1){
+                this.error = "You must enter a name";
+                this.showError = true;
+                return false
+            }
+            if(this.names.includes(name)){
+                this.error = "Names must be unique"
+                this.showError = true;
+                return false
+            }
+            
             return true
+        },
+        removeName(index){
+            this.names.splice(index, 1)
+        },
+        showResults(){
+            this.state = false;
         }
     }
 }).mount('#app')
+
+
+
+
+
+
+
