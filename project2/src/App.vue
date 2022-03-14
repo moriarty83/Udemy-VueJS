e84260d242695e4d64a852267c9d58ddc18ea235
moriarty83 <moriarty83@gmail.com>
2022-03-14 09:51:55 -0400

    Componenets, connecting

--- project2/src/App.vue
+++ project2/src/App.vue
@@ -1,17 +1,28 @@
 <template>
-    <h1>Hello {{name}}</h1>
+    <div>
+    <app-header></app-header>
+    <h2>Hello {{name}}</h2>
+    <comp-footer></comp-footer>
+    </div>
 </template>
     
 <script>
+    import Footer from './components/Footer.vue'
     export default{
         data(){
             return{
                 name: 'Chris',
             }
+        },
+        components:{
+            'comp-footer': Footer,
         }
     }
 
 </script>
 
 <style>
+    h1 {
+        color: red;
+    }
 </style>
--- project2/src/main.js
+++ project2/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from "vue";
 import App from './App.vue'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-createApp(App).mount('#app')
+
+const app = createApp(App);
+
+app.component('app-header',Header)
+app.component('app-footer', Footer)
+app.mount('#app')
