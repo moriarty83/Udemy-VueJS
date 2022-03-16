e68c6c6dea3dbe5b87d397311da42dc2227e9cff
moriarty83 <moriarty83@gmail.com>
2022-03-15 22:08:17 -0400

    Components completed

---
+++ project2/src/components/Directives/indexDirective.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div v-user-lastname="lastname">
+        
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            lastname: "jones"
+        }
+    },
+    directives:{
+        'user-lastname':{
+            beforeMount(el, binding) {
+                el.innerHTML = binding.value;
+            },
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
--- project2/src/main.js
+++ project2/src/main.js
@@ -9,11 +9,22 @@ import Footer from './components/Footer'
 const app = createApp(App);
 
 app.directive('awesome',{
+    created(el){
+        console.log("created")
+
+        console.log(el.parentNode)
+    },
+
     beforeMount(el,binding,vnode){
         el.innerHTML = binding.value;
         // el.style.color = binding.arg;
         el.style.color  = binding.modifiers.red ? 'red' : binding.modifiers.blue ? 'blue' : 'black';
         el.style.fontSize = binding.modifiers.big ? '20px' : binding.modifiers.small ? '10px' : '15px'
+    },
+    mounted(el, vnode){
+        console.log(el.parentNode)
+        console.log(vnode)
+
     },
 });
 app.component('app-header',Header);
