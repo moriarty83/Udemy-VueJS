54079800224eda7c0128a947573ae2adbffaa5e8
moriarty83 <moriarty83@gmail.com>
2022-03-18 13:42:09 -0400

    Routing, basics

---
+++ Routing/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <app-header></app-header>
+        <div class="container">
+            <router-view>
+              
+            </router-view>
+        </div>
+    <app-footer></app-footer>
+</template>
+
+<script>
+  // import HomeComponent from './components/index';
+  // import ArticlesMain from './components/Articles'
+  // import ArticleComp from './components/Articles/article'
+
+  export default{
+    components:{
+      // HomeComponent
+      // ArticlesMain
+      // ArticleComp
+    }
+  }
+</script>
---
+++ Routing/src/components/header_footer/Header.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <span class="fs-4">Beardit.com</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+          <!-- <a href="#" class="nav-link active" aria-current="page">Home</a> -->
+        </li>
+        <li class="nav-item">
+          <router-link to="/articles" class="nav-link">Articles</router-link>
+          <!-- <a href="#" class="nav-link">Articles</a> -->
+        </li>
+        <li class="nav-item">
+          <router-link to="/contact" class="nav-link">Contact</router-link>
+          <!-- <a href="#" class="nav-link">Contact</a> -->
+        </li>
+      </ul>
+    </header>
+  </div>
+</template>
+
+
+<style>
+    body {
+       padding:0;
+       margin:0;
+       font-family: 'Roboto', sans-serif;
+   }
+
+
+</style>
